18900b6a7fcc0e24d5226195d772c3f0a5108650
Amit Kumar <amitmonu0000@gmail.com>
2024-03-05 23:21:45 +0530

    update res.header

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,13 @@
 const isAuth = (req, res, next) => {
-  return req.session.isAuth
-    ? next()
-    : res.status(401).json("Session Expired, please log in again");
-//   if(req.session.isAuth) {
-//       next();
-//   } else {
-//       return res.status(401).json("Session Expired, please log in again");
-//   }
+  // return req.session.isAuth
+  //   ? next()
+  //   : res.status(401).json("Session Expired, please log in again");
+  if (req.session.isAuth) {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  } else {
+    return res.status(401).json("Session Expired, please log in again");
+  }
 };
 
 module.exports = { isAuth };
