5651b574edc1e60020b90c84a700d09a5b36562c
Amit Kumar <amitmonu0000@gmail.com>
2024-03-06 21:41:37 +0530

    remove console statments

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,11 +3,7 @@ const isAuth = (req, res, next) => {
   //   ? next()
   //   : res.status(401).json("Session Expired, please log in again");
   if (req.session.isAuth) {
-    console.log("isAuth", req.session.isAuth);
-    res.header("Access-Control-Allow-Credentials", true);
     next();
-    console.log(next);
-    console.log("after next");
   } else {
     return res.status(401).json("Session Expired, please log in again");
   }
